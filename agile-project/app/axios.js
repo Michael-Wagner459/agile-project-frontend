4be12d553af5f5e5f890d57ce06129db35a50417
michael-wagner459 <wagner.michael4592@gmail.com>
2024-08-06 18:56:52 -0700

    changed axios call to env

--- agile-project/app/axios.js
+++ agile-project/app/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //setting this up allows the user to not worry about the base url when using the axios method as it includes it in here
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 export default instance;
